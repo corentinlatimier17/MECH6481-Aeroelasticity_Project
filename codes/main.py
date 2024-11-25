47828e407a98531ef192c0dcfeed75f1869465e9
corentin.latimier17 <corentin.latimier17@gmail.com>
2024-11-24 20:39:49 -0500

    strange_behavior of pk-method

--- codes/main.py
+++ codes/main.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pmethod import *
+from pkmethod import *
 
 # Constants definition
 RHO_0 = 1.225 # Density at sea level [kg/m³]
 R = 287.058 # Temperature of at sea level [K]
 T0 = 288.15 # Specific gas constant [J/(kg.K)]
 G = 9.80665  # Gravitational constant [m/s²]
-
-# Regulation 
-alpha = 1.15
 
 def flight_envelope(h):
     # This function returns the minimum and maximum true speed for a given altitude h [km]
@@ -86,6 +85,148 @@ def plot_flight_envelopes():
 
     # Show plot
     plt.show()
+
+# --------------------------------------- Parameters ------------------------------------- #
+
+# Regulation parameters
+alpha = 1.15
+
+# -------------------- Wing parameters ---------------------- #
+
+# Team parameters (team 5 - see Excel sheet)
+S = 17.0 # wing area (whole wing) [m²]
+AR = 7.55 # Aspect ratio of the wing
+l = np.sqrt(AR*S)/2 # Span (length) of the right/left wing
+
+me = 26.91*S # Empty mass of the whole wing
+m_fuel = 80 # mass of fuel carried in each wing (i.e right and left)
+
+Ip_max = 7 # Max inertia (max weight) of the right/left at point P [kg.m²]
+e_max = -0.3 # Position of the center of mass at max weight situation
+m_max = me/2 + m_fuel # max weight of the right/left wing
+
+Ip_min = 4 # Min inertia (min weight) of the right/left at point P [kg.m²]
+e_min = -0.1 # Position of the center of mass at min weight situation
+m_min = me/2 # min weight of the right/left wing
+
+a = -0.2 # Position of the elastic axis in all situations
+
+c = S/(2*l) # Chord length
+b = c/2 # See course
+
+EI = 2*10**5 # Bending rigidity of the wing [N.m²]
+GJ = 10**5 # Torsional rigidity of the wing [N.m²]
+
+# See appendix for these calculations
+omega_h_min = (1.8751)**2*np.sqrt(EI/(m_min*l**3))
+omega_h_max = (1.8751)**2*np.sqrt(EI/(m_max*l**3))
+
+omega_theta_min = np.pi/2*np.sqrt(GJ/(Ip_min*l))
+omega_theta_max = np.pi/2*np.sqrt(GJ/(Ip_max*l))
+
+# Print results
+print(f"{'Parameter':<30} {'Symbol':<15} {'Value':<15} {'Unit'}")
+print("-" * 65)
+print(f"{'Wing Area (whole wing)':<30} {'S':<15} {S:<15.2f} {'m²'}")
+print(f"{'Aspect Ratio':<30} {'AR':<15} {AR:<15.2f} {'-'}")
+print(f"{'Span (right/left wing)':<30} {'l':<15} {l:<15.2f} {'m'}")
+print(f"{'Chord Length':<30} {'c':<15} {c:<15.2f} {'m'}")
+print(f"{'Half-Chord':<30} {'b':<15} {b:<15.2f} {'m'}")
+print(f"{'Empty Mass (whole wing)':<30} {'m_e':<15} {me:<15.2f} {'kg'}")
+print(f"{'Mass of Fuel (each wing)':<30} {'m_fuel':<15} {m_fuel:<15.2f} {'kg'}")
+print(f"{'Max Mass (each wing)':<30} {'m_max':<15} {m_max:<15.2f} {'kg'}")
+print(f"{'Min Mass (each wing)':<30} {'m_min':<15} {m_min:<15.2f} {'kg'}")
+print(f"{'Elastic Axis Location':<30} {'a':<15} {a:<15.2f} {'-'}")
+print(f"{'Center of Mass (max)':<30} {'e_max':<15} {e_max:<15.2f} {'-'}")
+print(f"{'Center of Mass (min)':<30} {'e_min':<15} {e_min:<15.2f} {'-'}")
+print(f"{'Bending Rigidity':<30} {'EI':<15} {EI:<15.2e} {'N·m²'}")
+print(f"{'Torsional Rigidity':<30} {'GJ':<15} {GJ:<15.2e} {'N·m²'}")
+print(f"{'Inertia (max)':<30} {'Ip_max':<15} {Ip_max:<15.2f} {'kg·m²'}")
+print(f"{'Inertia (min)':<30} {'Ip_min':<15} {Ip_min:<15.2f} {'kg·m²'}")
+print(f"{'Plunging natural frequency (min)':<30} {'omega_h_min':<15} {omega_h_min:<15.2f} {'rad/s'}")
+print(f"{'Plunging natural frequency (max)':<30} {'omega_h_max':<15} {omega_h_max:<15.2f} {'rad/s'}")
+print(f"{'Pitching natural frequency (min)':<30} {'omega_theta_min':<15} {omega_theta_min:<15.2f} {'rad/s'}")
+print(f"{'Pitching natural frequency (max)':<30} {'omega_theta_max':<15} {omega_theta_max:<15.2f} {'rad/s'}")
+
+
+# --- Computation of dimensionless parameters for SEA LEVEL flutter velocity ----- #
+V_eq_max = get_V_eq(0.8)[1]/3.6 # [m/s]
+
+# Case 1 - min weight
+mu_min = m_min/(np.pi*RHO_0*b**2*l)
+r_min = np.sqrt(Ip_min/(m_min*b**2))
+sigma_min = omega_h_min/omega_theta_min
+
+# Case 2 - max weight
+mu_max = m_max/(np.pi*RHO_0*b**2*l)
+r_max = np.sqrt(Ip_max/(m_max*b**2))
+sigma_max = omega_h_max/omega_theta_max
+
+# Definition of the reduced velocity to explore
+V_vec = np.linspace(0.01,2,200)
+
+# Results with p-method and p-k method
+pmethod_SL_min = pmethod(a=a, e=e_min, mu=mu_min, r=r_min, sigma=sigma_min, V_vec=V_vec)
+pmethod_SL_max = pmethod(a=a, e=e_max, mu=mu_max, r=r_max, sigma=sigma_max, V_vec=V_vec)
+pkmethod_SL_min = pkmethod(a=a, e=e_min, mu=mu_min, r=r_min, sigma=sigma_min, V_vec=V_vec)
+pkmethod_SL_max = pkmethod(a=a, e=e_max, mu=mu_max, r=r_max, sigma=sigma_max, V_vec=V_vec, eps=10**(-8), k0=0.5)
+
+pmethod_SL_min.run()
+pmethod_SL_max.run()
+
+pkmethod_SL_min.run()
+pkmethod_SL_max.run()
+
+Vrf_min_pmethod, _ = pmethod_SL_min.find_flutter()
+Vrf_max_pmethod, _ = pmethod_SL_max.find_flutter()
+Vrf_max_pmethod = 1.290 # approximately, algorithm find_flutter does not work for this case
+
+Vrf_min_pkmethod, _ = pkmethod_SL_min.find_flutter()
+Vrf_max_pkmethod, _ = pkmethod_SL_max.find_flutter()
+
+# Convert reduced flutter speed into real speed 
+Vf_min_pmethod = Vrf_min_pmethod*b*omega_theta_min
+Vf_max_pmethod = Vrf_max_pmethod*b*omega_theta_max
+
+Vf_min_pkmethod = Vrf_min_pkmethod*b*omega_theta_min
+Vf_max_pkmethod = Vrf_max_pkmethod*b*omega_theta_max
+
+
+# Check and print for p-method
+print("\n")
+if Vf_min_pmethod > alpha * V_eq_max:
+    print(f"PMETHOD: Flutter velocity for MIN weight ({Vf_min_pmethod:.2f} m/s) respects regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+else:
+    print(f"PMETHOD: Flutter velocity for MIN weight ({Vf_min_pmethod:.2f} m/s) does NOT respect regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+
+if Vf_max_pmethod > alpha * V_eq_max:
+    print(f"PMETHOD: Flutter velocity for MAX weight ({Vf_max_pmethod:.2f} m/s) respects regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+else:
+    print(f"PMETHOD: Flutter velocity for MAX weight ({Vf_max_pmethod:.2f} m/s) does NOT respect regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+
+# Check and print for pk-method
+if Vf_min_pkmethod > alpha * V_eq_max:
+    print(f"PKMETHOD: Flutter velocity for MIN weight ({Vf_min_pkmethod:.2f} m/s) respects regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+else:
+    print(f"PKMETHOD: Flutter velocity for MIN weight ({Vf_min_pkmethod:.2f} m/s) does NOT respect regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+
+if Vf_max_pkmethod > alpha * V_eq_max:
+    print(f"PKMETHOD: Flutter velocity for MAX weight ({Vf_max_pkmethod:.2f} m/s) respects regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+else:
+    print(f"PKMETHOD: Flutter velocity for MAX weight ({Vf_max_pkmethod:.2f} m/s) does NOT respect regulation (>{alpha * V_eq_max:.2f} m/s).\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
